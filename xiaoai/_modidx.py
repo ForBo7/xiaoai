357e3b2c82ad4f366562b41d1463d39fdf3faf29
Salman Naqvi <salmanaqvi+github@icloud.com>
2024-09-28 11:46:51 +0800

    Lesson 14

--- xiaoai/_modidx.py
+++ xiaoai/_modidx.py
@@ -6,4 +6,8 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/ForBo7/xiaoai',
                 'lib_path': 'xiaoai'},
   'syms': { 'xiaoai.core': { 'xiaoai.core.collate_dict': ('core.html#collate_dict', 'xiaoai/core.py'),
-                             'xiaoai.core.inplace': ('core.html#inplace', 'xiaoai/core.py')}}}
+                             'xiaoai.core.get_grid': ('core.html#get_grid', 'xiaoai/core.py'),
+                             'xiaoai.core.inplace': ('core.html#inplace', 'xiaoai/core.py'),
+                             'xiaoai.core.show_image': ('core.html#show_image', 'xiaoai/core.py'),
+                             'xiaoai.core.show_images': ('core.html#show_images', 'xiaoai/core.py'),
+                             'xiaoai.core.subplots': ('core.html#subplots', 'xiaoai/core.py')}}}
